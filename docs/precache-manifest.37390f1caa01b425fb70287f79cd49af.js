994884031f391a96bafd06d79087b7539e6d8047
Daniel <daniel.web19@outlook.com>
2020-09-11 19:44:04 -0500

    docs agregado

---
+++ docs/precache-manifest.37390f1caa01b425fb70287f79cd49af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cb736dc645711c297c883908f88a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c468232b7c3835e0c47",
+    "url": "/static/css/main.cdcff1d2.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c468232b7c3835e0c47",
+    "url": "/static/js/main.eedbbc01.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
